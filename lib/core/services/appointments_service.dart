71210803435dd068026ef12ffdbce9f8a07d8d14
Ahmed Bassell <ahmedbassell@gmail.com>
2023-08-23 12:05:07 +0300

    Add new created appointment/patient to global array

--- lib/core/services/appointments_service.dart
+++ lib/core/services/appointments_service.dart
@@ -2,11 +2,14 @@ import 'package:omni_dent/core/models/appointment.dart';
 import 'package:omni_dent/core/models/patient.dart';
 
 class AppointmentsService {
+  static List<Appointment> appointmentsArray = [];
   Appointment createAppointment(
       String name, DateTime dateTime, Patient patient) {
     int id = _calculateNextId();
-    return Appointment.new(
+    Appointment new_appointment = Appointment.new(
         id: id, name: name, dateTime: dateTime, patient: patient);
+    appointmentsArray.add(new_appointment);
+    return new_appointment;
   }
 
   int _calculateNextId() {
--- lib/core/services/patients_service.dart
+++ lib/core/services/patients_service.dart
@@ -1,9 +1,13 @@
 import 'package:omni_dent/core/models/patient.dart';
 
 class PatientsService {
+  static List<Patient> patientsArray = [];
+
   Patient createPatient(String name) {
     int id = _calculateNextId();
-    return Patient.new(id: id, name: name);
+    Patient new_patient = Patient.new(id: id, name: name);
+    patientsArray.add(new_patient);
+    return new_patient;
   }
 
   int _calculateNextId() {
