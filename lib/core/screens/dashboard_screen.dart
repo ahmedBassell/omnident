f1a4687f9d72a4d7d9cdcf749b0b80487a57f6c1
Ahmed Bassell <ahmedbassell@gmail.com>
2023-09-09 17:56:32 +0300

    Update dashboard screen UI

--- lib/core/screens/dashboard_screen.dart
+++ lib/core/screens/dashboard_screen.dart
@@ -1,14 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
-import 'package:omni_dent/core/models/appointment.dart';
-import 'package:omni_dent/core/models/patient.dart';
-import 'package:omni_dent/core/services/appointments_service.dart';
-import 'package:omni_dent/core/services/patients_service.dart';
-import 'package:omni_dent/core/widgets/appointment_creation_form.dart';
-import 'package:omni_dent/core/widgets/patient_card.dart';
-import 'package:omni_dent/core/widgets/patient_creation_form.dart';
-import 'package:omni_dent/database/database.dart';
-import 'package:omni_dent/locations/widgets/location_creation_form.dart';
+import 'package:omni_dent/patients/screens/patient_screen.dart';
 
 class DashboardScreen extends StatefulWidget {
   @override
@@ -16,177 +7,163 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
-  PatientsService get _patientsService => GetIt.I<PatientsService>();
-  AppointmentsService get _appointmentsService =>
-      GetIt.I<AppointmentsService>();
-  List<AppointmentWithPatient> upcomingAppointments = [];
-  List<Patient> recentPatients = [];
+  // PatientsService get _patientsService => GetIt.I<PatientsService>();
+  // AppointmentsService get _appointmentsService =>
+  //     GetIt.I<AppointmentsService>();
+  // List<AppointmentWithPatient> upcomingAppointments = [];
+  // List<Patient> recentPatients = [];
 
   @override
   void initState() {
     super.initState();
-    List<Patient> patients = [];
-    _patientsService.getPatients().then((patients) {
-      setState(() {
-        recentPatients = patients;
-      });
-    });
-    _appointmentsService.getAppointments().then((data) {
-      setState(() {
-        upcomingAppointments = data;
-      });
-    });
+    // List<Patient> patients = [];
+    // _patientsService.getPatients().then((patients) {
+    //   setState(() {
+    //     recentPatients = patients;
+    //   });
+    // });
+    // _appointmentsService.getAppointments().then((data) {
+    //   setState(() {
+    //     upcomingAppointments = data;
+    //   });
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(vertical: 32.0, horizontal: 16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 16),
-            Text(
-              'Upcoming Appointments',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 16),
-            // Display a list of upcoming appointments with details
-            // if (upcomingAppointments.isEmpty)
-            //   Text(
-            //     'No recent patients.',
-            //     style: TextStyle(fontSize: 16, color: Colors.grey),
-            //   )
-            // else
-            //   for (var appointmentWithPatient in upcomingAppointments)
-            //     _buildAppointmentCard(appointmentWithPatient),
-            SizedBox(height: 32),
-            Text(
-              'Most Recent Patients',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 16),
-            // if (recentPatients.isEmpty)
-            //   Text(
-            //     'No recent patients.',
-            //     style: TextStyle(fontSize: 16, color: Colors.grey),
-            //   )
-            // else
-            //   _buildRecentPatientsSection(),
-          ],
+      appBar: AppBar(
+        title: Text('Dashboard'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _buildInsights(),
+              SizedBox(height: 16),
+              _buildRecentPatientsList(),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  void _showAddOptions(BuildContext context) {
-    showModalBottomSheet(
-      context: context,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+  Widget _buildInsights() {
+    return Column(children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _buildInsightCard(
+              icon: Icons.people,
+              label: 'Total Patients',
+              value: '250', // Replace with actual patient count
+              color: Colors.red.shade200),
+          _buildInsightCard(
+              icon: Icons.calendar_today,
+              label: 'Today\'s Appointments',
+              value: '15', // Replace with actual appointment count for today
+              color: Colors.teal.shade200),
+        ],
       ),
-      builder: (BuildContext context) {
-        return Container(
-          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 16),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ListTile(
-                onTap: () {
-                  showModalBottomSheet(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return PatientCreationForm();
-                    },
-                  ).whenComplete(() {
-                    Navigator.pop(context);
-                  });
-                },
-                title: Text('Add a patient'),
-                leading: Icon(Icons.person_add),
-              ),
-              SizedBox(height: 16),
-              ListTile(
-                onTap: () {
-                  showModalBottomSheet(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AppointmentCreationForm();
-                    },
-                  ).whenComplete(() {
-                    Navigator.pop(context);
-                  });
-                },
-                title: Text('Create New Appointment'),
-                leading: Icon(Icons.calendar_today),
-              ),
-              // SizedBox(height: 16),
-              // ListTile(
-              //   onTap: () {
-              //     showModalBottomSheet(
-              //       context: context,
-              //       builder: (BuildContext context) {
-              //         return LocationCreationForm();
-              //       },
-              //     ).whenComplete(() {
-              //       Navigator.pop(context);
-              //     });
-              //   },
-              //   title: Text('Create New Location'),
-              //   leading: Icon(Icons.pin_drop),
-              // ),
-            ],
-          ),
-        );
-      },
-    ).whenComplete(() => {
-          // Navigator.pop(context);
-          setState(() async {
-            recentPatients = await _patientsService.getPatients();
-            upcomingAppointments = await _appointmentsService.getAppointments();
-            print(upcomingAppointments);
-          })
-        });
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _buildInsightCard(
+              icon: Icons.location_on,
+              label: 'Locations',
+              value: '3', // Replace with actual location count
+              color: Colors.teal.shade200),
+          _buildInsightCard(
+              icon: Icons.track_changes,
+              label: 'Tracked Instruments',
+              value: '50', // Replace with actual instrument count
+              color: Colors.red.shade200),
+        ],
+      )
+    ]);
   }
 
-  Widget _buildRecentPatientsSection() {
-    return Container(
-      height: 200,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: recentPatients.length,
-        itemBuilder: (context, index) {
-          var patient = recentPatients[index];
-          return PatientCard(patient);
-        },
+  Widget _buildInsightCard(
+      {required IconData icon,
+      required String label,
+      required String value,
+      required Color color}) {
+    return Expanded(
+        child: Card(
+      elevation: 2,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Icon(
+              icon,
+              size: 32,
+              color: color,
+            ),
+            SizedBox(height: 8),
+            Text(
+              label,
+              style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 4),
+            Text(
+              value,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
       ),
+    ));
+  }
+
+  Widget _buildRecentPatientsList() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          'Recent Patients',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 16),
+        // Replace the following list with your actual list of recent patients
+        _buildPatientListItem(
+            'John Doe', '30 years', 'Male', Colors.teal.shade200),
+        _buildPatientListItem(
+            'Jane Smith', '25 years', 'Female', Colors.red.shade200),
+        _buildPatientListItem(
+            'Alice Johnson', '45 years', 'Female', Colors.teal.shade200),
+        _buildPatientListItem(
+            'Bob Brown', '38 years', 'Male', Colors.red.shade200),
+        _buildPatientListItem(
+            'Eva White', '29 years', 'Female', Colors.teal.shade200),
+      ],
     );
   }
 
-  Widget _buildAppointmentCard(AppointmentWithPatient appointmentWithPatient) {
-    final appointment = appointmentWithPatient.appointment;
-    final patient = appointmentWithPatient.patient;
+  Widget _buildPatientListItem(
+      String name, String age, String gender, Color bgColor) {
     return Card(
       elevation: 2,
       margin: EdgeInsets.only(bottom: 16),
       child: ListTile(
-        title: Text(patient.name),
-        subtitle: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(appointment.title),
-            Text(
-              'Date: ${appointment.dateTimeFrom.day}/${appointment.dateTimeFrom.month}/${appointment.dateTimeFrom.year}',
-            ),
-            Text(
-              'Time: ${appointment.dateTimeFrom.hour}:${appointment.dateTimeFrom.minute}',
-            ),
-            Text(
-              'Gender: ${patient.gender.name}',
-            ),
-          ],
+        title: Text(name),
+        subtitle: Text('$age | $gender'),
+        leading: CircleAvatar(
+          backgroundColor: bgColor,
+          foregroundColor: Colors.white,
+          child: Text(name[0].toUpperCase()),
         ),
+        // Add onTap functionality to navigate to patient details if needed
+        onTap: () {
+          // Navigate to patient details page
+          // Navigator.push(
+          //     context,
+          //     MaterialPageRoute(
+          //         builder: (context) => PatientScreen(patient: _patient)));
+        },
       ),
     );
   }
